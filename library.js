aee692f4492120f180f6fcae09738feed65e3050
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2023-09-08 14:33:18 -0400

    Add edit and delete button images to each row

--- library.js
+++ library.js
@@ -14,5 +14,13 @@ Book.prototype.info = function () {
 };
 
 function addBookToLibrary(title, author, pages, read) {
-  myLibrary.push(new Book());
+  myLibrary.push(new Book(title, author, pages, read));
+}
+
+function getBookInfoFromUser() {
+  let newBookTitle = prompt('Enter title of the book');
+  let newBookAuthor = prompt("Enter author's name");
+  let newBookPages = +prompt('Enter the number of pages in the book');
+  let hasRead = prompt('Have your read this book?');
+  myLibrary.push();
 }
