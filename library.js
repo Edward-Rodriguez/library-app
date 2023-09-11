ab675ca57624de05d9b784488aa5d48e161aab1e
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2023-09-11 11:50:38 -0400

    CAdd function to display books and table cell data of each book

--- library.js
+++ library.js
@@ -17,6 +17,41 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
 }
 
+function displayBooks() {
+  const tableBody = document.querySelector('tbody');
+  myLibrary.forEach((book) => {
+    const newBook = document.createElement('tr');
+    newBook.appendChild(createTableData(book.title));
+    newBook.appendChild(createTableData(book.author));
+    newBook.appendChild(createTableData(book.pages));
+    newBook.appendChild(createTableData(book.read));
+    newBook.appendChild(generateEditButtons());
+    tableBody.appendChild(newBook);
+  });
+}
+
+function createTableData(bookInfo) {
+  const newBookTableData = document.createElement('td');
+  newBookTableData.textContent = bookInfo;
+  return newBookTableData;
+}
+
+function generateEditButtons() {
+  const buttonContainer = document.createElement('td');
+  buttonContainer.classList.add('buttons');
+  const editButton = document.createElement('img');
+  editButton.setAttribute('class', 'icon-edit');
+  editButton.setAttribute('src', 'icons/icon_edit-blue.svg');
+  editButton.setAttribute('alt', 'edit pencil');
+  const deleteButton = document.createElement('img');
+  deleteButton.setAttribute('class', 'icon-delete');
+  deleteButton.setAttribute('src', 'icons/icon_delete-blue.svg');
+  deleteButton.setAttribute('alt', 'recycling bin');
+  buttonContainer.appendChild(editButton);
+  buttonContainer.appendChild(deleteButton);
+  return buttonContainer;
+}
+
 function getBookInfoFromUser() {
   let newBookTitle = prompt('Enter title of the book');
   let newBookAuthor = prompt("Enter author's name");
@@ -24,3 +59,6 @@ function getBookInfoFromUser() {
   let hasRead = prompt('Have your read this book?');
   myLibrary.push();
 }
+
+addBookToLibrary('Testttitle', 'McAuthor', 6000, true);
+displayBooks();
