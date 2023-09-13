5e848571c61827dd3e253053fda906ab2fc6c4de
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2023-09-13 11:54:49 -0400

    Refactor

--- library.js
+++ library.js
@@ -1,15 +1,21 @@
 const myLibrary = [];
 const addEditBookDialog = document.querySelector('#addEditBookDialog');
 const addBookButton = document.querySelector('.add-button');
-const submitButton = document.querySelector('#ok-btn');
-const inputTextFields = document.querySelectorAll("input:not([type='radio'])");
-const radioButtons = document.querySelectorAll("input[type='radio']");
-const deleteButtons = document.querySelectorAll('.icon-delete');
-const cancelButton = document.querySelector('#cancel-btn');
+
+// Form Objects
 const form = document.querySelector('#addEditBookForm');
+const titleInput = document.querySelector("input[name='title']");
+const authorInput = document.querySelector("input[name='author']");
+const pagesInput = document.querySelector("input[name='pages']");
+const readInput = document.querySelectorAll("input[type='radio']");
+const cancelButton = document.querySelector('#cancel-btn');
+const submitButton = document.querySelector('#ok-btn');
+
+let isEditingMode = false;
+let bookIdToEdit = null;
 let bookId = 0;
 
-function Book(title, author, pages, read = false) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -17,42 +23,38 @@ function Book(title, author, pages, read = false) {
   this.id = ++bookId;
 }
 
-Book.prototype.info = function () {
-  return `${this.title} by ${author}, ${this.pages} pages, ${
-    this.read ? 'Read' : ''
-  }`;
-};
-
-function addBookToLibrary(newBook) {
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
-  addBookToTable(newBook);
 }
 
-function addBookToTable(book) {
+// to refresh entire table
+function updateTable() {
   const tableBody = document.querySelector('tbody');
-  const newBook = document.createElement('tr');
-  newBook.setAttribute('data-book-id', book.id);
-  newBook.appendChild(createTableData(book.title));
-  newBook.appendChild(createTableData(book.author));
-  newBook.appendChild(createTableData(book.pages));
-  newBook.appendChild(createTableData(book.read));
-  newBook.appendChild(initButtons());
-  tableBody.appendChild(newBook);
+  tableBody.textContent = ''; // clear all rows, remove all children
+  myLibrary.forEach((book) => {
+    const bookRow = document.createElement('tr');
+    bookRow.setAttribute('data-book-id', book.id);
+    bookRow.appendChild(createTableCellData(book.title));
+    bookRow.appendChild(createTableCellData(book.author));
+    bookRow.appendChild(createTableCellData(book.pages));
+    bookRow.appendChild(createTableCellData(book.read));
+    bookRow.appendChild(initEditDeleteButtons());
+    tableBody.appendChild(bookRow);
+  });
 }
 
-function createTableData(bookInfo) {
+function createTableCellData(bookInfo) {
   const newBookTableData = document.createElement('td');
   newBookTableData.textContent = bookInfo;
   return newBookTableData;
 }
 
-function initButtons() {
+function initEditDeleteButtons() {
   const buttonContainer = document.createElement('td');
   buttonContainer.classList.add('buttons');
-  const editButton = generateEditButton();
-  const deleteButton = generateDeleteButton();
-  buttonContainer.appendChild(editButton);
-  buttonContainer.appendChild(deleteButton);
+  buttonContainer.appendChild(generateEditButton());
+  buttonContainer.appendChild(generateDeleteButton());
   return buttonContainer;
 }
 
@@ -61,6 +63,7 @@ function generateEditButton() {
   editButton.setAttribute('class', 'icon-edit');
   editButton.setAttribute('src', 'icons/icon_edit-blue.svg');
   editButton.setAttribute('alt', 'edit pencil');
+  editButton.addEventListener('click', (ev) => handleEditButtonClick(ev));
   return editButton;
 }
 
@@ -75,70 +78,108 @@ function generateDeleteButton() {
 
 function onSubmit(ev) {
   ev.preventDefault();
-  let tempBook = new Book();
-  let isValidInput = true;
-  [...inputTextFields].forEach((input) => {
-    if (input.value.trim() === '') {
-      isValidInput = false;
-      input.classList.add('error');
-    } else {
-      tempBook[input.name] = input.value;
-      input.classList.remove('error');
-    }
-  });
-  tempBook.read = setReadStatus();
-  if (isValidInput) {
-    addBookToLibrary(tempBook);
+  if (validateForm()) {
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const pages = pagesInput.value;
+    const read = getReadStatus();
+    isEditingMode
+      ? updateBook(title, author, pages, read)
+      : addBookToLibrary(title, author, pages, read);
+    updateTable();
     addEditBookDialog.close();
+    isEditingMode = false;
+    bookIdToEdit = null;
     form.reset();
   }
 }
 
-function setReadStatus() {
-  let userchoice = [...radioButtons].find((btn) => btn.checked).value;
+function validateForm() {
+  isValid = true;
+  [titleInput, authorInput, pagesInput].forEach((input) => {
+    if (
+      input.value.trim() === '' ||
+      (input.name === 'pages' && isNaN(input.value))
+    ) {
+      isValid = false;
+      input.classList.add('error');
+    } else input.classList.remove('error');
+  });
+  return isValid;
+}
+
+function updateBook(title, author, pages, read) {
+  myLibrary.forEach((book) => {
+    if (book.id === bookIdToEdit) {
+      book.title = title;
+      book.author = author;
+      book.pages = pages;
+      book.read = read;
+    }
+  });
+}
+
+function getReadStatus() {
+  let userchoice = [...readInput].find((btn) => btn.checked).value;
   switch (userchoice) {
-    case 'not-read':
-    case false:
-      return null;
-    case 'in-progress':
-      return 'In Progress';
-    case 'read':
-      return 'Read';
+    case 'Not Started':
+      return null; //don't display status of non-read books
+    default:
+      return userchoice;
   }
 }
 
-function removeBookFromLibrary(bookToRemove) {
-  const indexOfBook = myLibrary.findIndex(
-    (book) => book.id === bookToRemove.id
-  );
+function removeBookFromTable(ev) {
+  const parentTableRow = getTableRowParent(ev.target);
+  const bookIdToRemove = +parentTableRow.getAttribute('data-book-id');
+  parentTableRow.parentElement.removeChild(parentTableRow);
+  removeBookFromLibrary(bookIdToRemove);
+}
+
+function removeBookFromLibrary(bookId) {
+  const indexOfBook = myLibrary.findIndex((book) => book.id === bookId);
   myLibrary.splice(indexOfBook, 1);
 }
 
-function removeBookFromTable(ev) {
-  const parentTableRow = ev.target.parentElement.parentElement;
-  const bookIdToRemove = +parentTableRow.getAttribute('data-book-id');
-  parentTableRow.parentElement.removeChild(parentTableRow);
-  removeBookFromLibrary(myLibrary.find((book) => book.id == bookIdToRemove));
+function handleEditButtonClick(ev) {
+  const bookId = +getTableRowParent(ev.target).getAttribute('data-book-id');
+  const bookToUpdate = myLibrary.find((book) => book.id === bookId);
+  [titleInput, authorInput, pagesInput].forEach((input) => {
+    input.value = bookToUpdate[input.name];
+    input.classList.remove('error');
+  });
+  [...readInput].forEach((input) => {
+    if (bookToUpdate.read === null && input.id === 'not-read')
+      input.checked = true;
+    else {
+      input.value === bookToUpdate.read
+        ? (input.checked = true)
+        : (input.checked = false);
+    }
+  });
+  isEditingMode = true;
+  bookIdToEdit = bookId;
+  addEditBookDialog.showModal();
 }
 
-addBookButton.addEventListener('click', () => {
-  form.reset();
-  addEditBookDialog.showModal();
-});
+function getTableRowParent(node) {
+  return node.closest('tr[data-book-id]');
+}
+
+addBookButton.addEventListener('click', () => addEditBookDialog.showModal());
 cancelButton.addEventListener('click', (ev) => {
   ev.preventDefault();
   addEditBookDialog.close();
 });
 submitButton.addEventListener('click', (ev) => onSubmit(ev));
 
+addBookToLibrary("Man's Search for Meaning", 'Viktor E. Frankl', 200, 'Read');
 addBookToLibrary(
-  new Book("Man's Search for Meaning", 'Viktor E. Frankl', 200, 'Read')
+  'Self-Compassion: The Proven Power of Being Kind to Yourself',
+  'Kristin Neff',
+  329,
+  'In Progress'
 );
-addBookToLibrary(
-  new Book(
-    'Self-Compassion: The Proven Power of Being Kind to Yourself',
-    'Kristin Neff',
-    329,
-    'In Progress'
-  )
-);
+addBookToLibrary('Test', 'John Doe', 319, 'Read');
+addBookToLibrary('Red Planet', 'Dr.John', 999, null);
+updateTable();
